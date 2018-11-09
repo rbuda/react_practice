bacb2e54a9af0d6678561d0895c649d36109c68c
rnebuda <rnebuda@conversantmedia.com>
2018-11-09 13:44:43 -0600

    early work render

--- treehouse/scoreboard/app.js
+++ treehouse/scoreboard/app.js
@@ -1,20 +1,14 @@
-const title = React.createElement(
-  'h1',
-  { id: 'main-title', title: 'This is a title.' },
-  'My First React Element!'
-);
+const title = 'My First React Element!';
+const desc = 'I just learned how to create a React node and render it into the DOM.';
+const myTitleID = 'main-title';
+const name = 'Ryan';
 
-const desc = React.createElement(
-  'p',
-  null,
-  'I just learned how to create a React node and render it into the DOM.'
-);
-
-const header = React.createElement(
-  'header',
-  null,
-  title,
-  desc
+const header = (
+  <header>
+  { /* This is how you comment in JSX */ }
+    <h1 id = { myTitleID }>{ name }'s First React Element!</h1>
+    <p className="main-desc">{ desc }</p>
+  </header>
 );
 
 ReactDOM.render(
